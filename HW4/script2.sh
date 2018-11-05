209122c64bf2a256a973d0d8e37523d0d2e8e792
Vimal Patel <vsp180000@cslinux1.utdallas.edu>
2018-11-04 23:44:27 -0600

    added: HW4

--- HW4/script2.sh
+++ HW4/script2.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 echo "Flie Created In 4.1 is following:"
@@ -6,8 +7,10 @@ ls dir2
 ls file1
 ls link1
 
+chmod 700 $HOME/HW4_vsp180000/HW4/script3.sh
 echo "Script3.sh contains the command:"
-cat ../script3.sh
+cat $HOME/HW4_vsp180000/HW4/script3.sh
 
-echo "New contents after running Script3.sh"
-../script3.sh
+
+echo "New contents after running Script3.sh:"
+$HOME/HW4_vsp180000/HW4/script3.sh
